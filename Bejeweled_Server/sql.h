a9e96390315069a53ccc5ed61e2da45745a32be8
ShenHua <2937746306@qq.com>
2024-12-09 18:13:53 +0800

    update: 项目架构

---
+++ Bejeweled_Server/sql.h
@@ -0,0 +1,39 @@
+#ifndef SQL_H
+#define SQL_H
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
+#include <QDebug>
+#include <QMessageBox>
+#include <QDir>
+
+class sql
+{
+public:
+    sql();
+
+    //析构函数
+    ~sql();
+    //连接到数据库
+    bool connectToDatabase();
+
+    //注册时检测用户名是否存在(返回1代表可以注册,0代表用户名已存在,-1代表用户名格式不对,-2代表密码格式不对)
+    int canRegisterOrNot(QString name, QString password);
+    //保存用户注册的账号信息
+    void saveUserRegisterData(QString name, QString password);
+    //登入时检测是否能登入(返回1代表能够登入,0代表密码错误, -1代表用户名不存在)
+    int canLoginOrNot(QString name, QString password);
+
+    //获取对局数据
+
+    //保存玩家游戏记录
+    void savePlayerGameRecord();
+private:
+    bool m_connectOrNot;
+    //数据库
+    QSqlDatabase m_db;
+};
+
+#endif // SQL_H
