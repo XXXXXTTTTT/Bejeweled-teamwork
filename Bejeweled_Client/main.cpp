f66af6863dd7e917e28d4ef56c752e9704e398c0
XXXXXTTTTT <144320307+XXXXXTTTTT@users.noreply.github.com>
2024-12-16 20:27:41 +0800

    Change: 修改main测试部分

--- Bejeweled_Client/main.cpp
+++ Bejeweled_Client/main.cpp
@@ -9,8 +9,7 @@ int main(int argc, char *argv[])
     MainWindow w;
 
 
-    ClientThread clientThread("127.0.0.1", 12345);
-    clientThread.start();
+
 
     w.show();
 
@@ -18,48 +17,51 @@ int main(int argc, char *argv[])
 
     //测试代码
 
-    //bgm播放
-    music m=music();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("start_timi.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("click.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("match.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("combo_2.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("combo_3.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("combo_4.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("combo_5.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("combo_6.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("combo_7.wav");
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("great.mp3");
+    // ClientThread clientThread("127.0.0.1", 12345);
+    // clientThread.start();
+    
+    // //bgm播放
+    // music m=music();
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("start_timi.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("click.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("match.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("combo_2.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("combo_3.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("combo_4.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("combo_5.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("combo_6.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("combo_7.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("great.mp3");
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("unbelievable.mp3");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("unbelievable.mp3");
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    //音效播放
-    m.sound("fail.wav");
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
+    // //音效播放
+    // m.sound("fail.wav");
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
 
     //停止
     // m.stop();
