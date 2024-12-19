ed5dbc70fa10c4a8ee69e85ba4f253f77b7d0a38
xdj66643 <142580490+xdj66643@users.noreply.github.com>
2024-12-20 00:25:25 +0800

    change: 修改pk图片大小

--- Bejeweled_Client/board.h
+++ Bejeweled_Client/board.h
@@ -12,7 +12,9 @@ class Board : public QObject{
 public:
     Board(int j[8][8] ,QGraphicsScene *scene);
     void generateBoard();
-    void updateBoard(int j[8][8]);
+    void updateBoard();
+    bool checkForInvalidPlacement(int x, int y, int gemType);
+    int getJewelType(int x, int y) const;
 
 private:
     std::vector<std::vector<int>> m_grid; //存储每个格子中宝石的类型
@@ -20,6 +22,13 @@ private:
     QGridLayout *m_gridLayout;
     const std::vector<int> m_JewelType = {1,2,3,4,5,6,7}; //7种宝石
     int m_j[8][8];
+
+    bool isBoardValid();
+    void clearBoard();   // 清空棋盘
+
+    bool checkHorizontal(int x, int y);
+    bool checkVertical(int x, int y);
 };
+
 
 #endif // BOARD_H
